5ae9fdb90ad0d7b87cfaeae6b679f93cd1213182
suresh <subhramanianadaviyar@gmail.com>
2024-01-07 11:13:47 +0530

    AWS commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_g5wmo0hNO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'h9jgtisd2lsg9rodrbd4s4ded', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://uovtnlwpt8.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
